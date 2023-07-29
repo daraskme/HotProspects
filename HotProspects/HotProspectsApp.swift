2e4f0a5db1220a69c3d6017d7d1c625de8db9899
DARASKdot <102633287+DARASKdot@users.noreply.github.com>
2023-07-29 19:02:01 +0900

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Hiroshi Inoue on R 5/07/29.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
