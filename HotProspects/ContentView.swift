e214902ec66eda530591a8abab1b00fe3e4b7e05
daraskme <ardysdibwa0qorgeg@icloud.com>
2023-08-02 20:00:18 +0900

    Learn 83days of swift 100days project 16

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -2,64 +2,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var output = ""
-    @State private var isLoading = true
-
-    var body: some View {
-        ZStack {
-            
-            Image("example")
-                .interpolation(.none)
-                .resizable()
-                .scaledToFit()
-                .frame(maxHeight: .infinity)
-                .background(.black)
-                .ignoresSafeArea()
-            
-            VStack {
-                if isLoading {
-                    Text("Loading...")
-                        .foregroundColor(.white)
-                    // ローディング中のメッセージ
-                } else {
-                    Text(output)
-                        .foregroundColor(.white)
-                    // 実際の結果を表示するメッセージ
-                }
-            }
-            .task {
-                await fetchReadings()
-                isLoading = false // 非同期処理が終わったらローディング中のメッセージを非表示にする
-            }
-        }
-    }
     
-    func fetchReadings() async { //asyncで非同期関数として定義
-        isLoading = true
-        let fetchTask = Task { () -> String in
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url) //URLから非同期でjsonをDL
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings"
+    @StateObject var prospects = Prospects()
+    
+    var body: some View {
+        TabView {
+            
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Label("Contected", systemImage: "checkmark.circle")
+                }
+            
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Label("UnContacted",systemImage: "questionmark.diamond")
+                }
+            
+            MeView()
+                .tabItem {
+                    Label("Me",systemImage: "person.crop.square")
+                }
         }
-        
-        let result = await fetchTask.result //関数の実行結果を代入する
-        
-        do {
-            output = try result.get() //エラーの可能性があるコードをdoに入れる
-        } catch {
-            output = "Error: \(error.localizedDescription)" //エラーの際の処理内容
-        }
-        
-        isLoading = false
-        
-//        switch result {
-//            case .success(let str):
-//                output = str
-//            case .failure(let error):
-//                output = "Error: \(error.localizedDescription)"
-//        }
-// do catchとほぼ同じ内容
+        .environmentObject(prospects)
     }
 }
 
