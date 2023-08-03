3ab61ccf314d5e3f63c6af2c2782b9331fa71c32
daraskme <ardysdibwa0qorgeg@icloud.com>
2023-08-03 19:57:19 +0900

    Learn all contents  swift 100days project 16

--- HotProspects/Prospect.swift
+++ HotProspects/Prospect.swift
@@ -2,21 +2,44 @@
 import SwiftUI
 
 class Prospect: Identifiable, Codable {
+    
     var id = UUID()
     var name = "Anonymous"
     var emailAddress = ""
+    
     fileprivate(set) var isContacted = false
 }
 
 @MainActor class Prospects: ObservableObject {
-    @Published var people:[Prospect]
+    
+    let saveKey = "SavedData"
+    @Published private(set) var people: [Prospect]
     
     init() {
+        if let data = UserDefaults.standard.data(forKey: saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                people = decoded
+                return
+            }
+        }
         people = []
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: saveKey)
+        }
     }
     
     func toggle(_ prospect: Prospect) {
         objectWillChange.send()
         prospect.isContacted.toggle()
+        save()
     }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
+    }
+    
 }
